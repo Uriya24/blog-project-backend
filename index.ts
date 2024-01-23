6e89d2a5647e7b88d1b2b4085ae88031bb8961af
uriya <uriya.binshtock@gmail.com>
2024-01-23 15:24:40 +0200

    changed from proxy to cors

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 // import activityLogger from './middlewares/activityLogger';
 import postsRoute from './routes/postsRoute';
 
 export const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
 // app.use(activityLogger);
 app.use("/api/posts", postsRoute);
 
